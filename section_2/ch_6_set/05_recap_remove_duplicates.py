bcd2a7ad1f5530bb9d91e35b21e7276a5b8d1f69
dkishorkoirala <dkishorkoirala@gmail.com>
2025-08-03 16:37:30 +0545

    here is set

---
+++ section_2/ch_6_set/05_recap_remove_duplicates.py
@@ -0,0 +1,30 @@
+"""
+Recap - Remove Duplicates
+
+In Python, you can convert a list to a set using the set() function. This is called casting. Casting to a set transforms the list into a set, which contains only unique elements.
+
+Example:
+
+my_list = [1, 2, 2, 3, 4, 4, 5]
+my_set = set(my_list)
+print(my_set)
+# Output: {1, 2, 3, 4, 5}
+
+Challenge
+
+Write a function named remove_duplicates that takes a list of numbers as an argument and returns a new list with duplicates removed while preserving the original order of elements.
+
+Example Input:
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+Example Output:
+
+[1, 2, 3, 4, 5]
+Use the properties of sets to simplify your solution while keeping the order intact!
+"""
+
+
+def remove_duplicates(numbers):
+    # Write code here
+    s = set(numbers)
+    return list(s)
